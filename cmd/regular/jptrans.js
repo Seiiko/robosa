16c059dde5b0772981b3d55fd7d00eb27186087b
Seiiko <seiko.eleanor@gmail.com>
2018-04-01 16:10:45 +0100

    lmfao

--- cmd/regular/jptrans.js
+++ cmd/regular/jptrans.js
@@ -5,10 +5,9 @@ module.exports.run = async (client, message, args) => {
   if (!args)
     return message.channel.send(":interrobang:  |  You did not specify a valid message!\n:interrobang:  |  **Usage:** .jptrans [what to translate]")
 
-  const jpTrans = async () => {
-    const text = await translate(args, { from: 'en', to: 'ja' });
+  translate(args, 'ja').then(text => {
     message.channel.send(text);
-  };
+  });
 
 }
 
