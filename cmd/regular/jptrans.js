ab2999e9dffadbf3b49dc95e3ae12ea26914c4fb
Seiiko <seiko.eleanor@gmail.com>
2018-04-07 19:46:19 +0100

    JP?

--- cmd/regular/jptrans.js
+++ cmd/regular/jptrans.js
@@ -1,12 +1,12 @@
-const translate = require('translate');
+const translate = require('translate-api');
 
 module.exports.run = async (client, message, args) => {
 
   if (!args)
     return message.channel.send(":interrobang:  |  You did not specify a valid message!\n:interrobang:  |  **Usage:** .jptrans [what to translate]")
 
-  translate(args, 'ja').then(text => {
-    message.channel.send(text);
+  translate.getText(args,{to: 'jp'}).then(function(text){
+    message.channel.send(text)
   });
 
 }
